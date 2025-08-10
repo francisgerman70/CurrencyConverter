ad7372c3b1a6a4d8546bc0b3aed7e924896aa01a
francis german <40007172+francisgerman70@users.noreply.github.com>
2025-08-09 19:51:22 -0700

    Currency Converter app initial commit

--- CurrencyConverter/ContentView.swift
+++ CurrencyConverter/ContentView.swift
@@ -9,13 +9,77 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+       ZStack {
+            // Background Image
+           Image(.background)
+               .resizable()
+               .ignoresSafeArea()
+           
+           VStack {
+               // Pony Image
+               Image(.prancingpony)
+                   .resizable()
+                   .scaledToFit()
+                   .frame(height: 200)
+               
+               // Currency Exchange Text
+               Text("Currency Exchange")
+                   .font(.largeTitle)
+                   .fontWeight(.bold)
+                   .foregroundColor(.white)
+               
+               // Conversion Section
+               HStack {
+                   // left Conversion
+                   VStack {
+                       HStack {
+                           Image(.silverpiece)
+                               .resizable()
+                               .scaledToFit()
+                               .frame(width: 50, height: 33)
+                               .padding()
+                               .background(Color.black.opacity(0.2))
+                           Text("Silver Piece")
+                               .fontWeight(.light)
+                               .foregroundStyle(.white)
+                       }
+                       TextField("Amount", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                           .background()
+                   }
+                   // Equals sign from SF symbols
+                   Image(systemName: "equal")
+                       .font(.largeTitle)
+                       .foregroundStyle(.white)
+                       .symbolEffect(.pulse)
+                   
+                   // Right Conversion
+                   VStack {
+                       HStack {
+                           Image(.goldpiece)
+                               .resizable()
+                               .scaledToFit()
+                               .frame(width: 50,height: 33)
+                               .padding()
+                               .background(Color.black.opacity(0.2))
+                           Text("Gold Piece")
+                               .fontWeight(.light)
+                               .font(.headline)
+                               .foregroundStyle(.white)
+                       }
+                       TextField("Amount", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                           .background()
+                   }
+               }
+               Spacer()
+               Image(systemName: "info.circle.fill")
+                   .padding(.leading, 300.0)
+                   .font(.largeTitle)
+                   .foregroundStyle(.white)
+                   .symbolEffect(.pulse)
+           }
+//           .padding(.bottom, 60.0)
+//           .border(.blue )
         }
-        .padding()
     }
 }
 
